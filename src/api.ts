e059c662f81702d183ee0b296bed8408fd31cfeb
KeranenJukka <kerppa26@gmail.com>
2019-12-09 13:17:14 +0200

    changes

--- src/api.ts
+++ src/api.ts
@@ -7,22 +7,19 @@ import {issueToken, validateToken} from './accounts/jwt';
 import {hashPassword, comparePassword} from './accounts/password';
 
 
-
 export default (router: express.Router) => {
 
 
+/*-------------- Serve the site ------------------*/
 
-/* -------- CORS- ----------*/
+router.use(express.static('thesite'));
 
-router.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+router.get('/', function (req, res) {
+  res.sendFile('index.html');
 });
 
-router.use(express.json())
-  
 
+  
 /* -------- Get user and id from token - ----------*/
 
   const getIdandUser = async (arg: string) => {
@@ -30,25 +27,35 @@ router.use(express.json())
     return new Promise((resolve, reject) => {
       
       var token = validateToken(arg);
+      
       token = JSON.parse(token["sub"])
-    
+      
       return resolve(token);
 
     })
     .catch(( ) => {
-      console.log("error")
+      
     })
 
   
   }  
 
 
+/* -------- CORS for development -----------*/
+
+    router.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+    
+    router.use(express.json())
+
 
 /* -------- Login ----------*/
 
 router.post('/login', async function (req, res) {
     
-var pass = req.body.password;
 
 await User.query().where("username", req.body.username)
 .then(resp => {
@@ -90,11 +97,9 @@ await User.query().where("username", req.body.username)
 
     })
     
-
-
-    
+  
 })
-.catch(err => {console.log(err)})
+.catch(() => {})
 
   
 })
@@ -141,10 +146,11 @@ var add = async (theinfo: object) => {
     //check id
     await User.query().where("username", info.username)
     .then((response) => {
-  
-    var id = response[0].id;
+    
       
     // get token
+
+    var id = response[0].id;
 
     var info = {
       username: req.body.username,
@@ -155,6 +161,8 @@ var add = async (theinfo: object) => {
     
     token = issueToken(token);
     
+    
+
     res.send({
       res: "success",
       token: token,
@@ -189,10 +197,10 @@ hashPassword(req.body.password)
 /* -------- Get user posts ----------*/
 
 router.post('/getposts', async (req, res) => {
-
+  
   getIdandUser(req.body.token)
   .then(async (info) => {
-
+    
     await Todo.query().where("userId", info["id"])
     .then((response) => {
   
@@ -211,8 +219,9 @@ router.post('/getposts', async (req, res) => {
   
 });
 
-/* -------- Add post ----------*/
 
+
+/* -------- Add post ----------*/
 
 router.post('/addpost', async (req, res) => {
 
@@ -242,6 +251,7 @@ router.post('/addpost', async (req, res) => {
     })
 
   })
+
 
 
 /* -------- Delete post ----------*/
@@ -274,6 +284,7 @@ router.post('/deletepost', async (req, res) => {
   });
 
 
+
   /* -------- Change post ----------*/
 
 
@@ -289,186 +300,23 @@ router.post('/changepost', async (req, res) => {
     .where("id", post)
     .where("userId", info["id"])
     .patch({description: req.body.text})
-    .then((response) => {
-      console.log(response)
+    .then(() => {
+      
       res.send("success")
     })
     .catch((err) => {
-      console.log(err)
+      
       res.send("error")
     })
 
 
    })
   .catch((err) => {
-    console.log(err)
+    
     res.send("error")
   })
-
  
   });
 
 
-/* -------- SOMETHING ----------*/
-
-
-router.post('/todos', async function (req, res) {
-    
-  console.log(req.body)
-
-  await Todo.query().insert(req.body)
-  .catch((err) => {});
- 
-  res.send('POST request to the homepage')
-})
-
-
-  router.get('/users', async (req, res) => {
-    const users = await User.query();
-    res.send(users);
-  });
-
-  router.get('/todos', async (req, res) => {
-    const todos = await Todo.query();
-    res.send(todos);
-  });
-
-
-};
-
-hashPassword("moimoi")
-
-.then((pass) => {
-
-var password = pass.hash
-
-
-
-setTimeout(() => {
-    
-  var n1 = {
-
-    username: "kerde",
-    firstname: "jukka",
-    lastname: "keränen",
-    password: password
-  
-  }
-  
-  async function jes () {
-    console.log("lisätään!")
-    const ins = await User.query().insert(n1)
-    .catch((err) => {
-      console.log(err)
-    })
-  
-  }
-  
-  jes()
-
-  var n2 = {
-
-    username: "kerdeman",
-    firstname: "meikä",
-    lastname: "keränen",
-    password: "jotain"
-  
-  }
-  
-  async function jes2 () {
-    console.log("lisätään!")
-    const ins = await User.query().insert(n2)
-    .catch((err) => {
-      console.log(err)
-    })
-  
-  }
-  
-  jes2()
-
-}, 3000);
-
-
-
-setTimeout(() => {
-  
-  var n3 = {
-
-    title: "post",
-    description: "tekstiä",
-    userId: 1,
-    checkbox: "no"
-      
-  }
-  
-  async function jes3 () {
-    console.log("lisätään tekstiä!")
-    const ins = await Todo.query().insert(n3)
-    .catch((err) => {
-      console.log(err)
-    })
-  
-  }
-
-jes3()
-
-var n4 = {
-
-  title: "postia",
-  description: "tekssdfdftiä",
-  userId: 2,
-  checkbox: "no"
-    
 }
-
-async function jes4 () {
-  console.log("lisätään tekstiä!")
-  const ins = await Todo.query().insert(n4)
-  .catch((err) => {
-    console.log(err)
-  })
-
-}
-
-jes4()
-
-
-var n5 = {
-
-  title: "postifasda",
-  description: "tekssdffdftiä",
-  userId: 1,
-  checkbox: "no"
-    
-}
-
-async function jes5 () {
-  console.log("lisätään tekstiä!")
-  const ins = await Todo.query().insert(n5)
-  .catch((err) => {
-    console.log(err)
-  })
-
-}
-
-jes5()
-
-
-}, 5000);
-
-})
-
-
-/*
-
-router.get('/todos/:id', async (req, res) => {
-  console.log(req.query)
-  const id = req.query.id;
-  if (!id || Number.isInteger(id)) throw new BadRequestError('Invalid TodoID!');
-
-  const todo = await Todo.query().where({ id }).first();
-  if (!todo) throw new NotFoundError('No such Todo!');
-  return todo;
-});
-
-*/
---
+++ thesite/precache-manifest.db554b8a25fbdcf522efad2f9dd7e897.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ae8e3b5c18af5b93dc7cafa2a309ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daa0ae99879b938a6f9a",
+    "url": "/static/css/main.13711aaa.chunk.css"
+  },
+  {
+    "revision": "285f6d22705f2d2a2e5d",
+    "url": "/static/js/2.650adcda.chunk.js"
+  },
+  {
+    "revision": "daa0ae99879b938a6f9a",
+    "url": "/static/js/main.46b8d02b.chunk.js"
+  },
+  {
+    "revision": "92f416338fa5b92447d6",
+    "url": "/static/js/runtime-main.79fbf0b5.js"
+  },
+  {
+    "revision": "73951c126f29ea132880c96229a6b713",
+    "url": "/static/media/mountains.73951c12.jpg"
+  },
+  {
+    "revision": "baf9542febb9a8ed99aa512b9dcb1757",
+    "url": "/static/media/note.baf9542f.png"
+  }
+]);
